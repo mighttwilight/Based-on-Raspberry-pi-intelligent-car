b9fdc51abef056cea780c6a80c4f6bf55cdeba72
mighttwilight <35017893+mighttwilight@users.noreply.github.com>
2018-01-02 10:48:54 +0800

    Smart car program
    Contains the serial port initial settings, infrared judgment obstacle avoidance, infrared remote control, computer remote control

---
+++ zxc.py
@@ -0,0 +1,90 @@
+import RPi.GPIO as GPIO
+import time
+from AlphaBot import AlphaBot
+
+Ab = AlphaBot()
+
+IR = 18
+n = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(IR,GPIO.IN,GPIO.PUD_UP)
+
+def getkey():
+	if GPIO.input(IR) == 0:
+		count = 0
+		while GPIO.input(IR) == 0 and count < 200:  #9ms
+			count += 1
+			time.sleep(0.00006)
+
+		count = 0
+		while GPIO.input(IR) == 1 and count < 80:  #4.5ms
+			count += 1
+			time.sleep(0.00006)
+
+		idx = 0
+		cnt = 0
+		data = [0,0,0,0]
+		for i in range(0,32):
+			count = 0
+			while GPIO.input(IR) == 0 and count < 15:    #0.56ms
+				count += 1
+				time.sleep(0.00006)
+				
+			count = 0
+			while GPIO.input(IR) == 1 and count < 40:   #0: 0.56mx
+				count += 1                               #1: 1.69ms
+				time.sleep(0.00006)
+				
+			if count > 8:
+				data[idx] |= 1<<cnt
+			if cnt == 7:
+				cnt = 0
+				idx += 1
+			else:
+				cnt += 1
+		print data
+		if data[0]+data[1] == 0xFF and data[2]+data[3] == 0xFF:  #check
+			return data[2]
+
+		if data[0] == 255 and data[1] == 255 and data[2] == 15 and data[3] == 255:
+			return "repeat"
+
+def reset():
+	while True:
+		key = getkey()
+		if(key != None):
+			if key == "repeat":
+				n = 0				
+			if key_== 0x18:
+				Ab.forward()
+		else:
+			n += 1
+			if n > 20000:
+				n = 0
+				Ab.forward()	
+#except KeyboardInterrupt:
+#	GPIO.cleanup();
+
+
+
+
+Ab.forward()
+try:
+	while True:
+		key = getkey()
+		if(key != None):
+			if key == "repeat":
+				n = 0				
+			if key == 0x1c:
+				Ab.stop()
+				reset()
+#				print("stop")
+		else:
+			n += 1
+			if n > 20000:
+				n = 0
+				Ab.forward()	
+except KeyboardInterrupt:
+	GPIO.cleanup();
